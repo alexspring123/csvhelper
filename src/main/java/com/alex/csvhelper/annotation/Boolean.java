d8892b766ff240618c565668453b02aa0adec14a
chengxinwen <chengxinwen@loukou.com>
2015-09-25 16:19:34 +0800

    修复bug

--- src/main/java/com/alex/csvhelper/annotation/Boolean.java
+++ src/main/java/com/alex/csvhelper/annotation/Boolean.java
@@ -1,19 +1,24 @@
 package com.alex.csvhelper.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.alex.csvhelper.validator.BooleanValidator;
+import com.alex.csvhelper.validator.ValidatorClass;
 
 /**
- * Created with IntelliJ IDEA.
- * User: alex
- * Date: 13-5-26
- * Time: 下午6:53
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: alex Date: 13-5-26 Time: 下午6:53 To change
+ * this template use File | Settings | File Templates.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(value = {ElementType.TYPE})
+@Target(value = { ElementType.FIELD })
+@ValidatorClass(value = BooleanValidator.class)
 public @interface Boolean {
-    String trueStr() default "是";
+	String trueStr() default "是";
 
-    String falseStr() default "否";
+	String falseStr() default "否";
 }
